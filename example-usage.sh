b7d64b48faceacb9686897640034d74be4526c9d
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-04-11 20:45:11 +0300

    example is updated

--- example-usage.sh
+++ example-usage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
 # in order to get John's mail address: 
-JOHN_MAIL=$(md-config -k "users.john doe.mail")
+JOHN_MAIL=$(config-md -i example-config.md -k "users.john doe.mail")
 
 echo "This is John's mail address: ${JOHN_MAIL}"
